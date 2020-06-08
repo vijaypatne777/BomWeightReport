047e094e2600756aeac4f9293bf297d10a1f209b
S0021712751 <sumanta.ghosh2@wipro.com>
2020-06-08 11:37:13 +0000

    Import content for version 1.0.8

---
+++ test/integration/arrangements/Startup.js
@@ -0,0 +1,1 @@
+sap.ui.define(["sap/ui/test/Opa5"],function(t){"use strict";return t.extend("com.bom.bomweight.test.integration.arrangements.Startup",{iStartMyApp:function(t){var a=t||{};a.delay=a.delay||50;this.iStartMyUIComponent({componentConfig:{name:"com.bom.bomweight",async:true},hash:a.hash,autoWait:a.autoWait})}})});
